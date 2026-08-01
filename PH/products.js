fca2afe263d498549aac2d3d97a9e92333baa06b
Shoplink-Stores <d87683326@gmail.com>
2026-08-02 05:33:01 +0800

    Create products.js

---
+++ PH/products.js
@@ -0,0 +1,18 @@
+const products = {
+
+    country: {
+
+        PH:"https://shoplink-stores.github.io/ARCELI-R/PH/shop.html",
+
+        QR:"https://shoplink-stores.github.io/ARCELI-R/QTR/shop.html",
+
+    },
+
+    product1: {
+
+        PH: " ",
+        QR: " ",
+
+    },
+
+};
