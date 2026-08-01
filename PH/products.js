3fe74d6fa2789db84ab3c121ffbcacbe0856aa1a
Shoplink-Stores <d87683326@gmail.com>
2026-08-02 05:38:29 +0800

    Update products.js

--- PH/products.js
+++ PH/products.js
@@ -4,14 +4,14 @@ const products = {
 
         PH:"https://shoplink-stores.github.io/ARCELI-R/PH/shop.html",
 
-        QR:"https://shoplink-stores.github.io/ARCELI-R/QTR/shop.html",
+        QA:"https://shoplink-stores.github.io/ARCELI-R/QTR/shop.html",
 
     },
 
     product1: {
 
         PH: " ",
-        QR: " ",
+        QA: " ",
 
     },
 
