1e636097d6cf68018f8344c34b5c654d1404a983
Shoplink-Stores <d87683326@gmail.com>
2026-08-02 11:57:41 +0800

    Update products.js

--- PH/products.js
+++ PH/products.js
@@ -10,8 +10,456 @@ const products = {
 
     product1: {
 
-        PH: " ",
-        QA: " ",
+        PH:"https://shop.jcpremiere.com/products/b0115d193f8145ba975bca69debf4950/11111510551",
+        QA:" ",
+
+    },
+
+    product2: {
+
+        PH:"",
+
+        QA:""
+
+    },
+
+    product3: {
+
+        PH:"",
+
+        QA:""
+
+    },
+
+    product4: {
+
+        PH:"",
+
+        QA:""
+
+    },
+
+    product5: {
+
+        PH:"",
+
+        QA:""
+
+    },
+
+    product6: {
+
+        PH:"",
+
+        QA:""
+
+    },
+
+    product7: {
+
+        PH:"",
+
+        QA:""
+
+    },
+
+    product8: {
+
+        PH:"",
+
+        QA:""
+
+    },
+
+    product9: {
+
+        PH:"",
+
+        QA:""
+
+    },
+
+    product10: {
+
+        PH:"",
+
+        QA:""
+
+    },
+
+    product11: {
+
+        PH:"",
+
+        QA:""
+
+    },
+
+    product12: {
+
+        PH:"",
+
+        QA:""
+
+    },
+
+    product13: {
+
+        PH:"",
+
+        QA:""
+
+    },
+
+    product14: {
+
+        PH:"",
+
+        QA:""
+
+    },
+
+    product15: {
+
+        PH:"",
+
+        QA:""
+
+    },
+
+    product16: {
+
+        PH:"",
+
+        QA:""
+
+    },
+
+    product17: {
+
+        PH:"",
+
+        QA:""
+
+    },
+
+    product18: {
+
+        PH:"",
+
+        QA:""
+
+    },
+
+    product19: {
+
+        PH:"",
+
+        QA:""
+
+    },
+
+    product20: {
+
+        PH:"",
+
+        QA:""
+
+    },
+
+    product21: {
+
+        PH:"",
+
+        QA:""
+
+    },
+
+    product22: {
+
+        PH:"",
+
+        QA:""
+
+    },
+
+    product23: {
+
+        PH:"",
+
+        QA:""
+
+    },
+
+    product24: {
+
+        PH:"",
+
+        QA:""
+
+    },
+
+    product25: {
+
+        PH:"",
+
+        QA:""
+
+    },
+
+    product26: {
+
+        PH:"",
+
+        QA:""
+
+    },
+
+    product27: {
+
+        PH:"",
+
+        QA:""
+
+    },
+
+    product28: {
+
+        PH:"",
+
+        QA:""
+
+    },
+
+    product29: {
+
+        PH:"",
+
+        QA:""
+
+    },
+
+    product30: {
+
+        PH:"",
+
+        QA:""
+
+    },
+
+    product31: {
+
+        PH:"",
+
+        QA:""
+
+    },
+
+    product32: {
+
+        PH:"",
+
+        QA:""
+
+    },
+
+    product33: {
+
+        PH:"",
+
+        QA:""
+
+    },
+
+    product34: {
+
+        PH:"",
+
+        QA:""
+
+    },
+
+    product35: {
+
+        PH:"",
+
+        QA:""
+
+    },
+
+    product36: {
+
+        PH:"",
+
+        QA:""
+
+    },
+
+    product37: {
+
+        PH:"",
+
+        QA:""
+
+    },
+
+    product38: {
+
+        PH:"",
+
+        QA:""
+
+    },
+
+    product39: {
+
+        PH:"",
+
+        QA:""
+
+    },
+
+    product40: {
+
+        PH:"",
+
+        QA:""
+
+    },
+
+    product41: {
+
+        PH:"",
+
+        QA:""
+
+    },
+
+    product42: {
+
+        PH:"",
+
+        QA:""
+
+    },
+
+    product43: {
+
+        PH:"",
+
+        QA:""
+
+    },
+
+    product44: {
+
+        PH:"",
+
+        QA:""
+
+    },
+
+    product45: {
+
+        PH:"",
+
+        QA:""
+
+    },
+
+    product46: {
+
+        PH:"",
+
+        QA:""
+
+    },
+
+    product47: {
+
+        PH:"",
+
+        QA:""
+
+    },
+
+    product48: {
+
+        PH:"",
+
+        QA:""
+
+    },
+
+    product49: {
+
+        PH:"",
+
+        QA:""
+
+    },
+
+    product50: {
+
+        PH:"",
+
+        QA:""
+
+    },
+
+    product51: {
+
+        PH:"",
+
+        QA:""
+
+    },
+
+    product52: {
+
+        PH:"",
+
+        QA:""
+
+    },
+
+    product53: {
+
+        PH:"",
+
+        QA:""
+
+    },
+
+    product54: {
+
+        PH:"",
+
+        QA:""
+
+    },
+
+    product55: {
+
+        PH:"",
+
+        QA:""
+
+    },
+
+    product56: {
+
+        PH:"",
+
+        QA:""
+
+    },
+
+    product57: {
+
+        PH:"",
+
+        QA:""
 
     },
 
